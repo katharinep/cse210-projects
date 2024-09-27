70cd445d61ba194df0235be11ae29c8528ea725f
katharinep <katharine.risingphoenix@gmail.com>
2024-09-27 17:01:31 -0600

    Completed 3 core requirements for the assignment.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,47 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+        
+        //Ask user for random number//
+        //Console.WriteLine("Choose a magic number:");
+        //string usernum = Console.ReadLine();
+        //int number = int.Parse(usernum);
+
+        //Generate magic number with random//
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1,101);
+
+        //Ask for user guess//
+        //Console.WriteLine("What is your guess?");
+        //string guess = Console.ReadLine();
+        //int intGuess = int.Parse(guess);
+        int guess = 0;
+
+        //Compare number//
+        while (guess != number)
+        {
+            //Ask for user guess//
+            Console.WriteLine("What is your guess?");
+            guess = int.Parse(Console.ReadLine());
+            //guess = int.Parse(guess);
+            
+            //Compare numbers//
+            if (guess == number)
+            {
+                Console.WriteLine("You guessed it!");
+            }
+            else if (guess > number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid integer.");
+            }
+        } 
     }
 }
